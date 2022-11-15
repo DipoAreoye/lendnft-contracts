964bb84b9100106aded9fb8e1233e26376ed2481
Brentably <Brent@pabnet.com>
2022-11-14 18:34:45 -0600

    gn

--- scripts/deployShit.ts
+++ scripts/deployShit.ts
@@ -30,55 +30,81 @@ const path = require('path')
 
 
 async function main() {
-  const [deployer1, deployer2] = await ethers.getSigners();
-  console.log(deployer1);
-  console.log(
-    "Deploying the contracts with the account:",
-    await deployer1.getAddress()
-  );
-  console.log(
-    "secondary account:",
-    await deployer2.getAddress()
-  );
+
+const [deployer1, deployer2] = await ethers.getSigners();
+console.log(deployer1);
+console.log(
+  "Deploying the contracts with the account:",
+  await deployer1.getAddress()
+);
+console.log(
+  "secondary account:",
+  await deployer2.getAddress()
+);
+
+// DEPLOY BAYC CONTRACT
+const BaycFactory = await ethers.getContractFactory(
+"contracts/BoredApeYachtClub.sol:BoredApeYachtClub"
+);
+const Bayc = await BaycFactory.deploy();
+await Bayc.deployed();
+
+console.log(`NFT's deployed at ${Bayc.address}`)
+
+// MINT NFT FROM deployer 2
+let baycTwo = Bayc.connect(deployer2) // connecting bayc with second signer
+
+let minted = await baycTwo.mintApe(1, {value: "1000000000000000"})
+const receipt = await minted.wait(1)
+console.log(receipt.status)
 
 
-  const BAYC = await ethers.getContractFactory(
-  "contracts/BoredApeYachtClub.sol:BoredApeYachtClub"
-  );
-  const bayc = await BAYC.deploy();
-  await bayc.deployed();
-  
-  console.log(`NFT's deployed at ${bayc.address}`)
-  
-  // console.log(bayc)
-  let baycTwo = bayc.connect(deployer2) // connecting two bayc with second signer
-  let minted = await baycTwo.mintApe(1, {value: "1000000000000000"})
-  const receipt = await minted.wait(1)
-  console.log(receipt.status)
-  
-  let IBayc = bayc.interface
-  console.log(IBayc)
-  let nft_from =  await deployer2.getAddress();
-  let nft_to = await deployer1.getAddress()
-  let tx_data = IBayc.encodeFunctionData("transferFrom", [
-    `${nft_from}`,
-    `${nft_to}`,
-    0
-  ])
-  console.log(tx_data)
+// dep1 deploys Summon contract
 
-  
-  let tx_nonce = await deployer2.getTransactionCount()
-  console.log(tx_nonce)
-  const nftTransferTxReq = {
-    to: bayc.address,
-    from: nft_from,
-    nonce: tx_nonce,
-    data: tx_data
-  }
+const SummonFactory = await ethers.getContractFactory(
+  "contracts/Summon.sol:Summon"
+)
+const Summon = await SummonFactory.deploy()
+await Summon.deployed()
 
-  const tx_resp = await deployer2.sendTransaction( nftTransferTxReq )
-  console.log(tx_resp)
+console.log(`Summon contract deployed at ${Summon.address}`)
+
+// dep2 calls setApprovalForAll, approving the summon contract as an operator
+
+
+
+// dep2 calls depositToken function on Summon contract
+
+
+
+
+
+
+// test for sending an NFT from deployer 2 to deployer 1 using a low level call
+  // let IBayc = bayc.interface
+  // console.log(IBayc)
+  // let nft_from =  await deployer2.getAddress();
+  // let nft_to = await deployer1.getAddress()
+  // let tx_data = IBayc.encodeFunctionData("transferFrom", [
+  //   `${nft_from}`,
+  //   `${nft_to}`,
+  //   0
+  // ])
+  // console.log(tx_data)
+  // let tx_nonce = await deployer2.getTransactionCount()
+  // console.log(tx_nonce)
+  // const nftTransferTxReq = {
+  //   to: bayc.address,
+  //   from: nft_from,
+  //   nonce: tx_nonce,
+  //   data: tx_data
+  // }
+  // const tx_resp = await deployer2.sendTransaction( nftTransferTxReq )
+  // console.log(tx_resp)
+
+
+
+
 
 }
   main().catch((error) => {
